2b1310efecfc0ee76b3687371147836c0e9f7f0e
Thomas12345678900 <scolatj@g.cofc.edu>
2022-01-21 14:57:00 -0500

    hw1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,44 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Scola
+GettingStarted.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: to form a program that does inputs and calculations
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area = ", area)
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Area = ", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shot = eval(input("Enter the player's total shots: "))
+    made_shot = eval(input("Enter the player's shots made: "))
+    shooting_percentage = ((made_shot)/(total_shot)) * 100
+    print("The player's shooting percentage is ", shooting_percentage)
 
 
 def coffee():
-    pass
+    pounds = eval(input("Enter how many pounds of coffee to be purchased: "))
+    price = (10.50 * pounds) + (0.86 * pounds) + 1.50
+    print("Your total costs are: ", price)
 
 
 def kilometers_to_miles():
-    pass
-
+    kilometers = eval(input("how many kilometers did you travel? "))
+    miles = kilometers / 1.61
+    print("That is",miles, "miles!")
 
 if __name__ == '__main__':
     pass
