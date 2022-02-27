98e3da22b77260588bdacf742164740a757516dc
Thomas12345678900 <scolatj@g.cofc.edu>
2022-02-26 19:25:55 -0500

    hw6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,65 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Scola
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: To create programs that utilize floats, encoding, and the return function
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
 
 
 def cash_converter():
-    pass
+    cash = float(input("Enter an integer: "))
+    print("That is ${:.2f}".format(cash))
 
 
 def encode():
-    pass
-
+    message_to_code = input("Enter a message")
+    key = eval(input("Enter a key: "))
+    string_collector = ""
+    for i in range(len(message_to_code)):
+        ordinal = ord(message_to_code[i])
+        total = key + ordinal
+        char = chr(total)
+        string_collector = string_collector + char
+    print(string_collector)
 
 def sphere_area(radius):
-    pass
+    area_sphere = 4 * (math.pi) * (radius**2)
+    return area_sphere
 
 
 def sphere_volume(radius):
-    pass
+    volume_sphere = 4/3 * (math.pi) * (radius**3)
+    return volume_sphere
 
 
 def sum_n(number):
-    pass
+    sum = number * ((number + 1)/2)
+    return sum
 
 
 def sum_n_cubes(number):
-    pass
+    num_cubes = (number * (number + 1)/2) **2
+    return num_cubes
 
 
 def encode_better():
-    pass
+    message_to_code = input('Enter your message to be coded: ')
+    key_word = input("enter the keyword")
+    total = ""
+    for i in range(len(message_to_code)):
+        message = ord(message_to_code[i]) - 65
+        key = ord(key_word[i % len(key_word)]) - 65
+        encoded_message = message + key
+        encoded_message = encoded_message % 58
+        final_answer = encoded_message + 65
+        finall_answer = chr(final_answer)
+        total = total + finall_answer
+
+    print(total)
 
 
 if __name__ == '__main__':
