43ab5b3ff2089174b80b21c0d7bbe785486d0d88
Thomas12345678900 <scolatj@g.cofc.edu>
2022-03-24 19:15:49 -0400

    lab9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -1,10 +1,14 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Scola
+lab9.py
+Certification of Authenticity:
+I worked on this assignment with Brooke Duvall at the CSL tutoring center.
 """
 
 
 def build_board():
+    board = [1,2,3,4,5,6,7,8,9]
+    return board
     pass
 
 
@@ -37,27 +41,113 @@ def print_board(board):
 
 
 def is_legal(board, position):
-    pass
+    if str(board[position-1]).isnumeric():
+        return True
+    else:
+        return False
 
 
 def fill_spot(board, position, character):
+    board[position-1] = character
+
     pass
 
+def game_is_won(board):
+    if board[0] == board[3] == board[6]:
+        return True
+    elif board[1] == board[4] == board[7]:
+        return True
+    elif board[2] == board[5] == board[8]:
+        return True
+    elif board[0] == board[1] == board[2]:
+        return True
+    elif board[3] == board[4] == board[5]:
+        return True
+    elif board[6] == board[7] == board[8]:
+        return True
+    elif board[0] == board[4] == board[8]:
+        return True
+    elif board[6] == board[4] == board[2]:
+        return True
+    else:
+        return False
+    pass
 
 def winning_game(board):
+    if game_is_won(board):
+        return True
+    else:
+        return False
     pass
-
 
 def game_over(board):
+    if game_is_won(board):
+        return True
+    x = 0
+    for i in board:
+        if not i.isnumeric():
+            x = x + 1
+    if x == 9:
+        return True
+    return False
+
     pass
 
-
 def get_winner(board):
+    if board[0] == board[3] == board[6] == 'x':
+        return 'x'
+    elif board[1] == board[4] == board[7] == 'x':
+        return 'x'
+    elif board[2] == board[5] == board[8] == 'x':
+        return 'x'
+    elif board[0] == board[1] == board[2] == 'x':
+        return 'x'
+    elif board[3] == board[4] == board[5] == 'x':
+        return 'x'
+    elif board[6] == board[7] == board[8] == 'x':
+        return 'x'
+    elif board[0] == board[4] == board[8] == 'x':
+        return 'x'
+    elif board[6] == board[4] == board[2] == 'x':
+        return 'x'
+    elif board[0] == board[3] == board[6] == 'o':
+        return 'o'
+    elif board[1] == board[4] == board[7] == 'o':
+        return 'o'
+    elif board[2] == board[5] == board[8] == 'o':
+        return 'o'
+    elif board[0] == board[1] == board[2] == 'o':
+        return 'o'
+    elif board[3] == board[4] == board[5] == 'o':
+        return 'o'
+    elif board[6] == board[7] == board[8] == 'o':
+        return 'o'
+    elif board[0] == board[4] == board[8] == 'o':
+        return 'o'
+    elif board[6] == board[4] == board[2] == 'o':
+        return 'o'
+    else:
+        return None
     pass
 
 
 def play(board):
-    pass
+    x_turn = True
+    char = "x"
+    while not game_over(board):
+        print_board(board)
+        if x_turn:
+            pos = eval(input('x position'))
+            char = "x"
+        else:
+            pos = eval(input('o position'))
+            char = "o"
+        if is_legal(board, pos):
+            fill_spot(board, pos, char)
+            x_turn = not x_turn
+
+    get_winner(board)
+
 
 
 def main():
